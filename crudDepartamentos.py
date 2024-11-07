515999638a8db6f7863ca03ff46644ee6186c679
Beatriz <beatrizfpeixoto@gmail.com>
2024-11-07 17:32:44 -0300

    metodo criar ok

--- crudDepartamentos.py
+++ crudDepartamentos.py
@@ -7,16 +7,14 @@ def criarDepartamento():
     Para criar DEPARTAMENTO, informe -> \b""")
     nome = input("Nome do departamento: \b") 
     gerente = input("Nome do gerente: \b") 
+    numero = 
 
-    with open("departamentos.txt", "w") as file: 
-        file.write(nome)
-        file.write("|")
-        file.write(gerente)
-        file.write("\b")
+    with open("departamentos.txt", "a") as file: 
+        file.write("\n" + nome + " | " + gerente + "\b")
 
-def verDepartamento(): 
-    #mostra todos os departamentos 
-    with open("", "r") as file:
+# def verDepartamento(): 
+#     #mostra todos os departamentos 
+#     with open("", "r") as file:
 
 
 # def apagarDepartamento(): 
