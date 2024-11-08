64dd285fe717f3fdff420dad5ecf18dc0b8e5831
Beatriz <beatrizfpeixoto@gmail.com>
2024-11-08 15:04:52 -0300

    aaa

--- crudDepProjeto.py
+++ crudDepProjeto.py
@@ -1,13 +1,13 @@
 import os 
 
-# def criarDepProjeto(): 
-#     print("""=== CRIAR PROJETO - DEPARTAMENTO === \b
-#     Para criar um projeto para um departamento, informe -> \b""")
-#     numeroProjeto = input("Número do projeto: \b")
-#     numeroDepartamento = input("Número do departamento: \b")
+def criarDepProjeto(): 
+    print("""=== CRIAR PROJETO - DEPARTAMENTO === \b
+    Para criar um projeto para um departamento, informe -> \b""")
+    numeroProjeto = input("Número do projeto: \b")
+    numeroDepartamento = input("Número do departamento: \b")
 
-#     with open("depProjetos.txt", "a") as file: 
-#         file.write("\n" + numeroProjeto + " | " + numeroDepartamento + "\b")
+    with open("depProjetos.txt", "a") as file: 
+        file.write("\n" + numeroProjeto + " | " + numeroDepartamento + "\b")
 
 def verDepProjetos(): 
     with open("depProjetos.txt", "r") as file: 
