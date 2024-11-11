607549d858b63659631b92922b2e3e10cb3485d9
DansLean <eudaniellcsilva@gmail.com>
2024-11-11 11:15:18 -0300

    editar relações adicionado

--- app/projects.py
+++ app/projects.py
@@ -33,30 +33,40 @@ def editarProjeto():
     listarProjetos()
 
     númeroProjetoEditar = input("Insira o número do projeto que deseja editar:\n> ")
-    itemProjetoEditar = int(input("\nInsira o item que deseja editar do projeto escolhido:\n0 - Número do Projeto\n1 - Nome do Projeto\n2 - Local do Projeto\n> "))
-    valorNovo = input("\nDigite o novo valor a ser inserido:\n> ")
-    quebraLinha = "\n"
-    foiEditado = False
+    itemProjetoEditar = int(input("\nInsira o item que deseja editar do projeto escolhido:\n0 - Número do Projeto\n1 - Nome do Projeto\n2 - Local do Projeto\n3- Funcionários\n4 - Departamentos\n> "))
 
-    for i,v in enumerate(projetos):
-        if v[0] == númeroProjetoEditar:
-            if itemProjetoEditar == 2:
-                v[itemProjetoEditar] = f"{valorNovo}{quebraLinha}"
-            v[itemProjetoEditar] = valorNovo
-            foiEditado = not foiEditado
-    
-    for i,v in enumerate(projetos):
-        projetos[i] = f"{v[0]}|{v[1]}|{v[2]}|\n"
-    
-    arquivo = open("tables/projetos.txt", "w")
-    arquivo.writelines(projetos)
-    
-    if foiEditado == True:
-        print("\nCampo editado com sucesso!\n")
+    if (itemProjetoEditar == 3):
+        quantidadeFuncionários = int(input("Insira quantos funcionários serão adicionados ao projeto:\n> "))
+        for i in range(quantidadeFuncionários):
+            númeroFuncionário = input("Insira o número do funcionário a ser adicionado ao projeto:\n> ")
+            relations.add_funcionario_projeto(númeroFuncionário, númeroProjetoEditar)
+        print("Funcionário(s) adicionado(s) com sucesso!")
+    elif (itemProjetoEditar == 4):
+        quantidadeDepartamentos = int(input("Insira quantos departamentos serão adicionados ao projeto:\n> "))
+        for i in range(quantidadeDepartamentos):
+            númeroDepartamento = input("Insira o número do departamento a ser adicionado ao projeto:\n> ")
+            relations.add_departamento_projeto(númeroDepartamento, númeroProjetoEditar)
+        print("Departamento(s) adicionado(s) com sucesso!")
     else:
-        print("\nCampo ou projeto não encontrado!\n")
-    
-    arquivo.close()
+        valorNovo = input("\nDigite o novo valor a ser inserido:\n> ")
+        foiEditado = False
+        for i,v in enumerate(projetos):
+            if v[0] == númeroProjetoEditar:
+                v[itemProjetoEditar] = valorNovo
+                foiEditado = not foiEditado
+        
+        for i,v in enumerate(projetos):
+            projetos[i] = f"{v[0]}|{v[1]}|{v[2]}|\n"
+        
+        arquivo = open("tables/projetos.txt", "w")
+        arquivo.writelines(projetos)
+        
+        if foiEditado == True:
+            print("\nCampo editado com sucesso!\n")
+        else:
+            print("\nCampo ou projeto não encontrado!\n")
+        
+        arquivo.close()
     pass
 
 def apagarProjeto():
